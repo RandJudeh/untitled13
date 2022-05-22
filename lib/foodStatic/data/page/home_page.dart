922f0d00bacab16a7862c1ebcdcbe13fdd583e12
RandJudeh <randjudeh@outlook.com>
2022-05-22 19:27:54 +0300

    kkkk

---
+++ lib/foodStatic/data/page/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:transition_location_ui/foodStatic/data/widget/locations_widget.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Color.fromARGB(245, 221, 205, 205),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Text(''),
+          centerTitle: true,
+          actions: [
+            // IconButton(
+            //   icon: Icon(Icons.location_pin),
+            //   onPressed: () {},
+            // ),
+          ],
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            color: Colors.black,
+            onPressed: () {},
+          ),
+        ),
+        body: LocationsWidget(),
+        bottomNavigationBar: buildBottomNavigation(),
+      );
+
+  Widget buildBottomNavigation() => BottomNavigationBar(
+        elevation: 0,
+        unselectedItemColor: Colors.white54,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.transparent,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pin_drop_outlined),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_location),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: '',
+          ),
+        ],
+      );
+}
